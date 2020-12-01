9e70e92c88a1d3fdc430790c3c07bacb8ee050b0
liurui <liurui@moseeker.com>
2020-12-01 13:21:42 +0800

    add add

--- git-test/src/Dev1.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author liurui
- * @date 2020/12/1 12:57 下午
- */
-public class Dev1 {
-    public static void main(String[] args) {
-        System.out.println("dev1");
-    }
-}
--- git-test/src/Dev111.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author liurui
- * @date 2020/12/1 1:16 下午
- */
-public class Dev111 {
-    public static void main(String[] args) {
-
-    }
-}
